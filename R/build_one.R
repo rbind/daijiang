7fc3fa0916b897287d51c438187d1286f4afa599
daijiang <daijianglee@gmail.com>
2017-05-21 00:41:15 -0400

    Digging too deep...

--- R/build_one.R
+++ R/build_one.R
@@ -18,11 +18,20 @@ local({
     base.url = '/', width = 60
   )
   knitr::knit(a[1], a[2], quiet = TRUE, encoding = 'UTF-8', envir = .GlobalEnv)
+
   if (file.exists(a[2])) {
     x = blogdown:::append_yaml(
       blogdown:::readUTF8(a[2]), list(from_Rmd = TRUE)
     )
     blogdown:::writeUTF8(xaringan:::protect_math(x), a[2])
-    Sys.chmod(a[2], '0444')  # read-only (should not edit)
+
+    x2 = blogdown:::split_yaml_body(blogdown:::readUTF8(a[2]))
+    blogdown:::render_page(a[2])
+    a2.html = gsub("[.]md$", ".html", a[2])
+    x3 = append(blogdown:::readUTF8(a2.html), x2$yaml, 0)
+    blogdown:::writeUTF8(x3, a2.html)
+
+    Sys.chmod(a2.html, '0444')  # read-only (should not edit)
+    unlink(a[2])
   }
 })
